35f2db875e1750916450f683bfa131a8fec805b0
TamaraCliment1920 <tamaracliment@gmail.com>
2019-12-16 10:45:19 +0100

    Actualizado Pedido

--- HMantenimiento/src/main/java/serpis/ad/Cliente.java
+++ HMantenimiento/src/main/java/serpis/ad/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Cliente {
@@ -12,6 +13,8 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
     private String nombre;
+    
+    @ManyToOne
 	
 	public Long getId() {
 		return id;
--- HMantenimiento/src/main/java/serpis/ad/Pedido.java
+++ HMantenimiento/src/main/java/serpis/ad/Pedido.java
@@ -1,9 +1,12 @@
 package serpis.ad;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.xml.crypto.Data;
 
 @Entity
 public class Pedido {
@@ -11,5 +14,36 @@ public class Pedido {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	private Data fecha;
+	private String nombre;
+	private BigDecimal importe;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId (Long id) {
+		this.id =id;
+	}
+	public Data getfecha() {
+		return fecha;
+		}
+	
+	public void setfecha (Data fecha) {
+		this.fecha =fecha;
+	}
+	public String getnombre() {
+		return nombre;
+		}
+	
+	public void setnombre (String nombre) {
+		this.nombre =nombre;
+	}
+	public BigDecimal getimporte() {
+		return importe;
+		}
+	
+	public void setimporte (BigDecimal importe) {
+		this.importe =importe;
+	}
 }
