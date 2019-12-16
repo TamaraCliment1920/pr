9d94cf063aaab7b9874b4ffb7796b93e9a8ea5dd
TamaraCliment1920 <tamaracliment@gmail.com>
2019-12-16 12:02:18 +0100

    Añadido Menu

--- HMantenimiento/src/main/java/serpis/ad/HibernateMain.java
+++ HMantenimiento/src/main/java/serpis/ad/HibernateMain.java
@@ -1,10 +1,24 @@
 package serpis.ad;
 
+import java.util.Scanner;
+
 public class HibernateMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner tcl=new Scanner(System.in);
+		System.out.println("Bienvenido");
+		System.out.println("¿Que vamos a Gestionar?");
+		System.out.println("1-Clientes");
+		System.out.println("2-Pedidos");
+		int op=tcl.nextInt();
+		if (op==1) {
+			System.out.println("Bienvenido a la Gestion de Clientes");
+		}
+		if (op==2) {
+			System.out.println("Bienvenido a la Gestion de Pedidos");
+		}
 	}
 
 }
