31fd842640a716b31e71404f5ef575606a92b0f7
TamaraCliment <tamaracliment@gmail.com>
2020-01-29 21:03:47 +0100

    BINGO

---
+++ CBingo/CBingo/CBingo/MainWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CBingo;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        Panel panel = new Panel(vbox1);
+        Bombo bombo = new Bombo();
+        BJugar.Clicked += delegate
+        {
+            int numero = bombo.sacarBola();
+            panel.Marcar(numero);
+        };
+    }
+
+
+     /*protected void OnBJugarClicked(object sender, EventArgs e)
+        {
+            
+        }*/
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+   
+}
